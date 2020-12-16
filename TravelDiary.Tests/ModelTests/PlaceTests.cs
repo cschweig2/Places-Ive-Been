7b84c2622ba71e042102b265e29407f347bd50ce
Tyler Sinks <tyler.sinks@gmail.com>
2020-12-16 16:11:26 -0500

    add spec for cityName field, achieve good fail
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- TravelDiary.Tests/ModelTests/PlaceTests.cs
+++ TravelDiary.Tests/ModelTests/PlaceTests.cs
@@ -11,10 +11,21 @@ namespace TravelDiary.Tests
     [TestMethod]
     public void CreateAPlace_ReturnsNewInstanceOfPlace_Place()
     {
-      Place newPlace = new Place();
+      Place newPlace = new Place("Frankfurt");
       Assert.AreEqual(typeof(Place), newPlace.GetType());
     }
 
-    
+    [TestMethod]
+    public void GivePlaceACityName_ReturnsInstanceWithCityName_CityName()
+    {
+      // Arrange
+      string cityName = "Philadelphia";
+      Place newPlace = new Place(cityName);
+      // Act
+      string result = newPlace.CityName;
+      // Assert
+      Assert.AreEqual("Philadelphia", result);
+    }
+
   }
 }
--- TravelDiary/Models/Place.cs
+++ TravelDiary/Models/Place.cs
@@ -4,6 +4,15 @@ namespace TravelDiary.Models
 {
     public class Place
     {
+        public string CityName;
+        private int id;
+
+        public Place(string cityName)
+        {
+            CityName = "Mumbai";
+            id = 0;
+        }
+
 
     }
 }
